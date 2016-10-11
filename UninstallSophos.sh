46f51f2c8133b444423eb56d7ec6b768844e5a8b
atscomposer <atscomposer@gmail.com>
2016-10-11 09:17:21 -0400

    Initial Upload

---
+++ UninstallSophos.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+################################################################################
+
+# Author: Adam Shuttleworth
+# Created: 2016-10-11
+# Modified: 2016-10-11
+# Name: UninstallSophos
+
+################################################################################
+
+sudo "/Library/Application Support/Sophos/saas/Installer.app/Contents/MacOS/tools/InstallationDeployer" --remove --tamper_password "{tamperPW}"
