38047cb07ebedbfccf4f2416529b28e74fc5b3de
Shuttleworth, Adam <ashuttleworth@irobot.com>
2017-06-14 15:45:08 -0400

    Deleted

--- Production/Uninstall All Managed HQ Copiers.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-lpstat -p | cut -d' ' -f2 | grep HQ_PRINTSRV | xargs -I{} lpadmin -x {}
-
-jamf recon
--- Production/Uninstall All Managed PAS Copiers.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-lpstat -p | cut -d' ' -f2 | grep PAS-DCW-01 | xargs -I{} lpadmin -x {}
-
-jamf recon
